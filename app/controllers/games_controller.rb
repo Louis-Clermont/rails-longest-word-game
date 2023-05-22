b80e926ecefcecfb4f21fb1af85fcb0180c1349e
Louis Clermont <l.clermont12@gmail.com>
2023-05-22 17:33:35 +0100

    longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,75 @@
+# require 'json'
+# require 'open-uri'
+
+# class GamesController < ApplicationController
+#   def new
+#     @letters = Array.new(10) { ('a'..'z').to_a.sample }
+#   end
+
+#   def uses_grid_letters_incorrectly?(attempt, grid)
+#     # Checks to see if an attempt uses any letters not in the grid,
+#     # or uses any letter more times than it appears in the grid.
+#     remaining_letters = grid
+#     attempt.upcase.chars.each do |letter|
+#       return true unless grid.include?(letter)
+#       remaining_letters.delete_at(remaining_letters.index(letter))
+#     end
+#     return false
+#   end
+
+#   def check_word_validity(word)
+#     url = "https://wagon-dictionary.herokuapp.com/#{word}"
+#     response = URI.open(url).read
+#     result = JSON.parse(response)
+#     result['found']
+#   end
+
+#   def score
+#     letters = params[:letters].split(' ')
+#     attempt = params[:attempt]
+
+#     if uses_grid_letters_incorrectly?(attempt, letters)
+#       @response = "Sorry, but #{attempt} cannot be built out of #{letters.join(' ')}."
+#     elsif check_word_validity(attempt)
+#       @response = "Sorry, but #{attempt} is not an English word."
+#     else
+#       @response = "Congratulations, #{attempt} is a valid word!"
+#     end
+#   end
+# end
+
+require 'net/http'
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+  def uses_grid_letters_incorrectly?(attempt, grid)
+    # Checks to see if an attempt uses any letters not in the grid,
+    # or uses any letter more times than it appears in the grid.
+    remaining_letters = grid
+    attempt.upcase.chars.each do |letter|
+      return true unless grid.include?(letter)
+      remaining_letters.delete_at(remaining_letters.index(letter))
+    end
+    return false
+  end
+  def invalid_english_word?(attempt)
+    # Use the Le Wagon dictionary to check if a word exists.
+    url = URI("https://wagon-dictionary.herokuapp.com/#{attempt}")
+    serialized_response = Net::HTTP.get(url)
+    dictionary_response = JSON.parse(serialized_response)
+    return !dictionary_response["found"]
+  end
+  def score
+    letters = params[:letters].split(' ')
+    attempt = params[:attempt]
+    if uses_grid_letters_incorrectly?(attempt, letters)
+      @response = "Sorry, but #{attempt} cannot be build out of #{letters.join(' ')}."
+    elsif invalid_english_word?(attempt)
+      @response = "Sorry, but #{attempt} is not an English word."
+    else
+      @response = "Congratulations, #{attempt} is a valid word!"
+      cookies[:score] = cookies[:score].to_i + 10
+    end
+  end
+end
